3d320908aa6a219d7b1c42c5624cf0c58af66aed
Alex Pitsikoulis <alex.pitsikoulis@gmail.com>
2019-05-15 19:13:34 -0400

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,7 +7,7 @@ var cards = [
     {
         rank: "queen",
         suit:  "diamonds",
-        cardImage: "images/queen-of-diamons.png"
+        cardImage: "images/queen-of-diamonds.png"
     },
     {
         rank: "king",
@@ -28,15 +28,24 @@ var checkForMatch = function() {
         alert("Sorry, try again.");
     }
 }
-var flipCard = function(cardId) {
-    console.log("User flipped " + cards[cardId].rank)
-    cardsInPlay.push(cards[cardId].rank)
+var flipCard = function() {
+    var cardId = this.getAttribute('data-id');
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
+    this.setAttribute('src', cards[cardId].cardImage);
     if (cardsInPlay.length === 2) {
         checkForMatch();
     }
-}
-flipCard(0);
-flipCard(2);
-
+};
+ createBoard = function() {
+    for (i=0;i<cards.length;i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+};
+createBoard();
